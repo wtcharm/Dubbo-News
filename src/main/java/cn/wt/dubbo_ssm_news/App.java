f69616d50671436c6c2a658d9daea1cf88b118fe
handsomewangtao <handsomethewt@163.com>
2017-10-28 19:01:20 +0800

    实现新闻新项目的父POM的搭建

---
+++ src/main/java/cn/wt/dubbo_ssm_news/App.java
@@ -0,0 +1,13 @@
+package cn.wt.dubbo_ssm_news;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
